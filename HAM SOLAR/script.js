d181eb593f158de231319248e96d7617a1106cc5
yashitakumari <94585854+yashitakumari@users.noreply.github.com>
2025-02-21 05:30:03 +0530

    Add files via upload

---
+++ HAM SOLAR/script.js
@@ -0,0 +1,59 @@
+// const scroll = new LocomotiveScroll({
+//                   el: document.querySelector('[#main]'),
+//                   smooth: true
+//  });
+
+// function page4Animation() {
+//                   var elemC = document.querySelector("#elem-container")
+//                   var fixed = document.querySelector("#fixed-image")
+//                   elemC.addEventListener("mouseenter", function () {
+//                       fixed.style.display = "block"
+//                   })
+//                   elemC.addEventListener("mouseleave", function () {
+//                       fixed.style.display = "none"
+//                   })
+              
+//                   var elems = document.querySelectorAll(".elem")
+//                   elems.forEach(function (e) {
+//                       e.addEventListener("mouseenter", function () {
+//                           var image = e.getAttribute("data-image")
+//                           fixed.style.backgroundImage = `url(${image})`
+//                       })
+//                   })
+//               }
+
+
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+          fixed.style.display = "block"            
+})
+elemC.addEventListener("mouseleave", function () {
+                 fixed.style.display = "none"
+ })
+
+ var elems = document.querySelectorAll(".elem") 
+      elems.forEach(function (e) {
+   e.addEventListener("mouseenter", function () {
+   var image = e.getAttribute("data-image")
+   fixed.style.backgroundImage = `url(${image})`
+    })
+ })
+
+ var menu = document.querySelector("nav h3")
+ var full = document.querySelector("#full-scr")
+ var naving = document.querySelector("nav img")
+ var flag = 0
+ menu.addEventListener("click", function(){
+   if(flag == 0){
+  full.style.top=0
+  naving.style.opacity = 0
+  flag = 1
+} else {
+  full.style.top= "-100%"    
+  naving.style.opacity = 1
+  flag = 0                             
+}
+ })
+ 
